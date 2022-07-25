42837fc362d86c461a2ae23d80f8ac5a95bac1b8
barath0211 <101321055+barath0211@users.noreply.github.com>
2022-07-25 13:22:19 +0530

    Updated JavaScript

--- code.js
+++ code.js
@@ -1,61 +1,89 @@
-// const addBox = document.querySelector(".container"),
-// myTodo = document.querySelector(".myTodo");
-// addBox.addEventListener("click",() => {
-//     myTodo.classList.add("display");
-// });
-// addBtm.addEventListener(click,e => {
-//     e.preventDefault();
-//     let noteTitle = title.value,
-//     noteDes = description.value;
-//     console.log(noteTitle,noteDes);
-// })
-const inputVal = document.getElementsByClassName('inputVal')[0];
- const addTaskBtn = document.getElementsByClassName('btn')[0];
-addTaskBtn.addEventListener('click', function (){
- if(inputVal.value.trim()!=0){
-      let localItems = JSON.parse( localStorage.getItem('localItem'))
-   if(localItems === null){
-        taskList = []
-   }
-   else{
-       taskList = localItems;
-   }
-   taskList.push(inputVal.value)
-   localStorage.setItem('localItem', JSON.stringify(taskList)); 
-}
-   showItem();
-})
-function showItem(){
-   let localItems = JSON.parse( localStorage.getItem('localItem'))
-   if(localItems === null)
-   {
-        taskList = []
-   }
-   else{
-       taskList = localItems;
-   }
-let html = '';
-let itemShow = document.querySelector('.todoLists');
-taskList.forEach((data, index )=> {
-   html += `
-   <div class="todoList">
-   <p class="pText">${data}</p> 
-   <button class="deleteTask" onClick="deleteItem(${index})">Delete</button>
-   <button class="editTask" onclick="editItem(${index},'${data}')">Edit</button> 
-   </div>
-   `
-})
-itemShow.innerHTML = html;
-}
-showItem();
-function deleteItem(index){
-   let localItems = JSON.parse( localStorage.getItem('localItem'))
-   taskList.splice(index, 1)
-   localStorage.setItem('localItem', JSON.stringify(taskList));
-   showItem()
-}
-// function editItem(${index},'${data}') 
-// {
-// console.log(data);
-// }
+const modalBox=document.querySelector(".modalBox");
+const addNote=document.querySelector("#takeText");
+const tagDescription=document.querySelector(".getDescription");
+const tagTitle=document.querySelector(".getTitle");
+const closeBtn=modalBox.querySelector("#closeBtn");
+let addBtn=modalBox.querySelector( "#addBtn");
 
+
+//SAVING NOTES
+const notes=JSON.parse(localStorage.getItem("notes") ||"[]");
+let update= false,updateId;
+addNote.addEventListener("click", (event) =>{
+    event.preventDefault();
+    tagTitle.focus();
+       modalBox.classList.add("show");
+});
+window.onclick = function(e){
+    if(e.target == modalBox){
+        modalBox.classList.remove("show")
+    }
+};
+closeBtn.addEventListener("click", () =>{ 
+    update=false;
+    tagTitle.value="";
+    tagDescription.value="";
+    addBtn.innerText= "add note";
+    modalBox.classList.remove("show");
+});
+
+
+
+function displayNotes(){
+    document.querySelectorAll(".note").forEach(note =>note.remove());
+    notes.forEach((note,index)=> {
+        let content=`<li class="note">
+                            <div class="content"><h4>${note.title}</h4><p>${note.description}</p>
+                            </div>
+                                <ul class="menu">
+                                    <li onclick="editNotes(${index},'${note.title}', '${note.description}')"><button class="editbtn">Edit </button></li>
+                                    <li onclick="deleteNotes(${index})"><button class="delete-btn">Delete</button></li>
+                                </ul>
+                            </div>
+    </li>`
+    addNote.insertAdjacentHTML("afterend",content);
+    });
+}
+ displayNotes();
+
+
+ //DELETE A NOTE
+function deleteNotes(noteId){
+     notes.splice(noteId,1);
+     localStorage.setItem("notes",JSON.stringify(notes));
+     displayNotes(); 
+}
+
+
+//EDIT A NOTE
+function editNotes(noteId,title,description){
+    update= true;
+    updateId=noteId;
+    tagTitle.value=title;
+    tagDescription.value=description;
+    addNote.click();
+    addBtn.innerText= "update";
+}
+
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    
+    let noteTitle=tagTitle.value;
+    let desc=tagDescription.value;
+    if(noteTitle || desc) {
+       let noteDetails = {
+               title:noteTitle,
+                description:desc
+        }
+      if(!update){
+        notes.push(noteDetails);
+      }else{
+        update=false;
+        notes[updateId] = noteDetails;
+      }
+       localStorage.setItem("notes",JSON.stringify(notes));
+       closeBtn.click();
+       displayNotes();
+    }
+});
